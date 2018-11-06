35fe09507a351bf0f76c4fc94eb43fc81206b978
Harry McKenzie <42568513+HarryMcKenzie@users.noreply.github.com>
2018-11-06 02:13:37 +0100

    pppoof

--- jungleBot.js
+++ jungleBot.js
@@ -1746,7 +1746,7 @@
                     if (this.type === 'exact' && chat.message.length !== cmd.length) return void(0);
                     if (!jungleBot.commands.executable(this.rank, chat)) return void(0);
                     else {
-                        API.sendChat('/me @' + chat.un + ' is now AFK :ResidentSleeper: ');
+                        API.sendChat('/me @' + chat.un + ' is now AFK :ppPoof: ');
       		          }
       		      }
       	    },
